1cc408e61177989f50f257abfa4fb32328e58b41
brkdnmz <brkdnmz99@gmail.com>
2023-12-28 03:44:45 +0300

    Silly bugfix

--- src/hooks/use-force-render.ts
+++ src/hooks/use-force-render.ts
@@ -3,7 +3,10 @@ import { useEffect, useState } from "react";
 export function useForceRender(seconds: number) {
   const [, setState] = useState(0);
   useEffect(() => {
-    const interval = setInterval(() => setState((prev) => prev + 1), seconds);
+    const interval = setInterval(
+      () => setState((prev) => prev + 1),
+      seconds * 1000, // My life had been demolished not multiplying by 1000 here, fork this shirt!
+    );
     return () => clearInterval(interval);
   }, [seconds]);
 }
